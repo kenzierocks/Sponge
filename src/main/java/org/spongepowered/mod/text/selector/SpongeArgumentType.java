0116fa355d4f927a788a2aea3a962f59002f48e0
Kenzie Togami <ket1999@gmail.com>
2015-04-17 23:09:14 -0700

    Brand new converter

--- src/main/java/org/spongepowered/mod/text/selector/SpongeArgumentType.java
+++ src/main/java/org/spongepowered/mod/text/selector/SpongeArgumentType.java
@@ -3,9 +3,10 @@ package org.spongepowered.mod.text.selector;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.collect.Maps;
-import org.apache.commons.lang3.NotImplementedException;
+import org.spongepowered.api.CatalogType;
 import org.spongepowered.api.text.selector.ArgumentType;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
+import org.spongepowered.mod.SpongeMod;
 
 import java.lang.reflect.Method;
 import java.util.Map;
@@ -25,8 +26,20 @@ public class SpongeArgumentType<T> extends SpongeArgumentHolder<ArgumentType<T>>
                 final Method valueOf = type.getMethod("valueOf", String.class);
                 converters.put(type, SpongeSelectorFactory.<String, T>methodAsFunction(valueOf, true));
             } catch (NoSuchMethodException ignored) {
-                // TODO handle category types here
-                throw new NotImplementedException("No @CategoryType support");
+                if (CatalogType.class.isAssignableFrom(type)) {
+                    final Class<? extends CatalogType> type2 = type.asSubclass(CatalogType.class);
+                    converters.put(type, new Function<String, T>() {
+
+                        @Override
+                        public T apply(String input) {
+                            // assume it exists for now
+                            return (T) SpongeMod.instance.getGame().getRegistry().getType(type2, input).get();
+                        }
+
+                    });
+                } else {
+                    throw new IllegalStateException("can't convert " + type);
+                }
             } catch (SecurityException ignored) {
             }
         }
